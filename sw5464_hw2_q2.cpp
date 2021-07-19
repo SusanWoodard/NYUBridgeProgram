df67163a778278c9b639fa248ec21f2855205e14
SusanWoodard <82958148+SusanWoodard@users.noreply.github.com>
2021-07-19 12:33:55 -0600

    Update sw5464_hw2_q2.cpp

--- sw5464_hw2_q2.cpp
+++ sw5464_hw2_q2.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 int main()
 {
-int dollars, cents;
-int quarters, dimes, nickels, pennies;
-int totalCents, minusQuarters, minusQuartersAndDimes, minusQuartersDimesAndNickels;
+	int dollars, cents = 0;
+	int quarters, dimes, nickels, pennies;
+	int totalCents, minusQuarters, minusQuartersAndDimes, minusQuartersDimesAndNickels;
 
+	cout << "Pleaes enter your amount in the format of dollars and cents separated by a space:" << endl;
+	cin >> dollars;
+	cin >> cents;
 
-cout >> "Please enter your amount in the format of dollars and cents separated by a space:" << endl;
-cin << dollars cents << endl;
+	totalCents = (dollars * 100) + cents;
 
-totalCents = (dollars * 100) + cents; //There are 100 cents in one dollar.
+	quarters = totalCents / 25;
+	minusQuarters = totalCents % 25;
+	dimes = minusQuarters / 10;
+	minusQuartersAndDimes = minusQuarters % 10;
+	nickels = minusQuartersAndDimes / 5;
+	minusQuartersDimesAndNickels = minusQuartersAndDimes % 5;
+	pennies = minusQuartersDimesAndNickels / 1;
 
-quarters = totalCents / 25; //There are 25 cents in one quarter.
-minusQuarters = totalCents % 25;
-dimes = minusQuarters / 10; //There are 10 cents in one dime.
-minusQuartersAndDimes = minusQuarters % 10;
-nickels = minusQuartersAndDimes / 5; //There are 5 cents in one nickel.
-minusQuartersDimesAndNickels = minusQuartersAndDimes % 5;
-pennies = minusQuartersDimesAndNickels / 1; //There is 1 cent in a penny.
+	cout << dollars << " dollars and " << cents << " cents are: " << endl;
+	cout << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, and " << pennies << " pennies.";
 
-cout>> <<dollars<< "dollars and " <<cents<< "cents are: " <<endl;
-cout>> <<quarters<< "quarters, " <<dimes<< "dimes, "<<nickels<<"nickels, and "<<pennies<<"pennies."
-
-return 0;
+	return 0;
 }
